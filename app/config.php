52c428a82876befc01f76fdc129f918f02e8f8d2
VOID001 <zhangjianqiu13@gmail.com>
2015-02-05 22:19:24 +0800

    Add config.php

---
+++ app/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: void-admin
+ * Date: 15-2-5
+ * Time: 下午9:33
+ * File: config.php
+ * Description: configuration file
+ */
+$db_host="localhost";
+$db_port="2333";
+$db_username="justQuiz";
+$db_password="quizByNEUP-NET";
+$db_name="justQuiz";
+
+$developer="VOID001";
