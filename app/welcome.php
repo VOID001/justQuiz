e5f0a70b2934fb9833f2899eb0fd5b5da2af0217
VOID001 <zhangjianqiu13@gmail.com>
2015-02-06 11:30:44 +0800

    主界面编写第一阶段完毕

--- app/welcome.php
+++ app/welcome.php
@@ -4,4 +4,41 @@
  * User: void-admin
  * Date: 15-2-5
  * Time: 下午10:18
- */ 
+ * File: welcome.php
+ * Description: The main UI for homepage
+ */
+
+?>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ 	<head>
+		<title>JustQuiz! An app for quizzing and examing</title>
+	</head>
+	<body>
+		<div style='margin: auto;font-family:KaiTi, "楷体", sans-serif;text-align: center;font-size:40px'>
+			justQuiz!一个测评和考试的App
+		</div>
+		<table style="margin:auto;">
+			<th></th>
+			<tr><br/></tr>
+			<tr>
+				<td>
+					<a href="quiz.php">
+						<div style="background-color:yellowgreen; width:300px;height:200px;">
+							<div style='margin:auto;font-family:"Courier New","Arial","sans-serif","monospace";font-size:40px;'>Take Quiz! 参加测试</div>
+						</div>
+					</a>
+				</td>
+				<!--
+				<td>
+					<div style="background-color:red; width:300px;height:200px;">
+						<a href="admin/admin.php">
+							<div style='margin:auto;font-family:"Courier New","Arial","sans-serif","monospace";font-size:40px;'>Add A Quiz!</div>
+						</a>
+					</div>
+				</td>
+				!-->
+			</tr>
+		</table>
+	</body>
+</html>
