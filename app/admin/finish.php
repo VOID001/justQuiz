c29fddf85b54103d98751ed0a48313bd49758055
VOID001 <zhangjianqiu13@gmail.com>
2015-02-06 13:28:39 +0800

    安装完成时跳转页面完成

---
+++ app/admin/finish.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: void-admin
+ * Date: 15-2-6
+ * Time: 上午11:03
+ */
+//rename("setup.php","setup.php_del");
+//rename("finish.php","finish.php_del");
+//echo "<html><head><meta http-equiv='refresh' content='3' url='../' ></head></html>";
+header("Location:../index.php");
+exit();
+
