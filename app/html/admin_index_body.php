d343f8f55bab612c5e56c4d7d75ed6de949fe3ea
VOID001 <zhangjianqiu13@gmail.com>
2015-02-15 14:36:00 +0800

    将静态html改为动态PHP文件

--- app/html/admin_index_body.php
+++ app/html/admin_index_body.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
-    <meta charset="UTF-8">
-    <title></title>
+	<?php require_once(dirname(__FILE__)."/../html/header.php");?>
+    <title>管理入口</title>
 </head>
 <body style="width: 100%;height: 768px">
+<?php require_once(dirname(__FILE__)."/navbar_top.php");?>
 <div style="top:50%;margin:auto;text-align:center">
     <div>
         <h2><a href="../admin/additem.php">试题添加</a></h2>
@@ -14,4 +15,5 @@
     </div>
 </div>
 </body>
+<?php require_once(dirname(__FILE__)."/../html/footer.php");?>
 </html>
